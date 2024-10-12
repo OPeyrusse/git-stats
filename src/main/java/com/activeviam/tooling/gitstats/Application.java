f755b0442f4f22d3939ff42a12e5f5d8fe8c0557
Olivier Peyrusse <ope@activeviam.com>
2024-10-12 14:24:45 +0200

    First commit

---
+++ src/main/java/com/activeviam/tooling/gitstats/Application.java
@@ -0,0 +1,17 @@
+/*
+ * (C) ActiveViam 2024
+ * ALL RIGHTS RESERVED. This material is the CONFIDENTIAL and PROPRIETARY
+ * property of ActiveViam. Any unauthorized use,
+ * reproduction or transfer of this material is strictly prohibited
+ */
+
+package com.activeviam.tooling.gitstats;
+
+/**
+ * @author ActiveViam
+ */
+public class Application {
+  public static void main(final String[] args) {
+    
+  }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,1 @@
+module com.activeviam.tooling.gitstats {}
