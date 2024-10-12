b4de51affe2d1def7c078ad9a2e9c116ff12dcc2
Olivier Peyrusse <ope@activeviam.com>
2024-10-12 19:52:30 +0200

    Hide module info for the moment

--- module-info.java
+++ module-info.java
@@ -2,7 +2,6 @@ module com.activeviam.tooling.gitstats {
   requires commons.cli;
   requires java.logging;
   requires io.opentelemetry.instrumentation_annotations;
-  requires org.mapstruct.processor;
   requires io.opentelemetry.api;
   requires org.apache.avro;
   requires parquet.hadoop;
