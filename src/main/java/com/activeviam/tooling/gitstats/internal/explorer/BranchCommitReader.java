d33eee1cecee305a9af6b736fec4613f4ff4e165
Olivier Peyrusse <ope@activeviam.com>
2024-10-13 21:16:17 +0200

    Correct reader to really read sha1 in range

--- src/main/java/com/activeviam/tooling/gitstats/internal/explorer/BranchCommitReader.java
+++ src/main/java/com/activeviam/tooling/gitstats/internal/explorer/BranchCommitReader.java
@@ -11,17 +11,24 @@ import com.activeviam.tooling.gitstats.internal.orchestration.Action;
 import com.activeviam.tooling.gitstats.internal.orchestration.Queue;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.instrumentation.annotations.WithSpan;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.logging.Level;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import lombok.val;
 
 /**
  * @author ActiveViam
  */
 @RequiredArgsConstructor
+@Log
 public class BranchCommitReader {
 
   private final Path projectDir;
@@ -33,6 +40,9 @@ public class BranchCommitReader {
   @Getter(lazy = true, value = lombok.AccessLevel.PRIVATE)
   private final String resolvedCommit = resolveStartCommit();
 
+  @Getter(lazy = true, value = lombok.AccessLevel.PRIVATE)
+  private final Set<String> commitsToIgnore = readCommitsToIgnore();
+
   public void run() {
     final var increment = 100;
     IntStream.iterate(0, i -> i < this.historySize, i -> i + increment)
@@ -42,16 +52,20 @@ public class BranchCommitReader {
 
   @WithSpan("Read branch commits")
   private void readCommits(final int start, final int increment) {
+    log.log(Level.INFO, "Reading commits from {0} to {1}", new Object[] {start, start + increment});
     Span.current().setAttribute("branch", this.branch);
     Span.current().setAttribute("start-commit", start);
     final var end = Math.min(start + increment, this.historySize);
     Span.current().setAttribute("end-commit", end);
 
     final var output =
-        Shell.execute(List.of("git", "cherry", commit(end), commit(start)), this.projectDir);
+        Shell.execute(
+            List.of("git", "log", "--format=%H", commit(end) + ".." + commit(start)),
+            this.projectDir);
     Shell.Output.readStream(output.stdout())
         .lines()
         .map(this::trimCommitLine)
+        .filter(Predicate.not(getCommitsToIgnore()::contains))
         .map(Action::value)
         .forEach(this.output::put);
   }
@@ -71,5 +85,21 @@ public class BranchCommitReader {
 
   private String trimCommitLine(final String line) {
     return line.replaceFirst("\\s*\\+\\s*", "");
+  }
+
+  private Set<String> readCommitsToIgnore() {
+    val ignoredFile = this.projectDir.resolve(".git-blame-ignore-revs");
+    if (Files.exists(ignoredFile)) {
+      try {
+        return Files.readAllLines(ignoredFile).stream()
+            .filter(Predicate.not(String::isBlank))
+            .filter(line -> !line.startsWith("#"))
+            .collect(Collectors.toUnmodifiableSet());
+      } catch (final Exception e) {
+        throw new RuntimeException("Failed to read ignored commits", e);
+      }
+    } else {
+      return Set.of();
+    }
   }
 }
